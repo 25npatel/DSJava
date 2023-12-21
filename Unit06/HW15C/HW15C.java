5c8999831c35c76b64b344963ee4a480d30f80f8
25npatel <25npatel@derryfield.org>
2023-12-20 23:49:11 -0500

    updated

---
+++ Unit06/HW15C/HW15C.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class HW15C {
+
+    public static int linearSearch (int[] array, int tgtVal) {
+
+        int searchedValue = 10000;
+        int pos = 0;
+        boolean found = false;
+        while (pos < array.length && !found) {
+            if (array[pos] == searchedValue) {
+                found = true;
+            } else {
+                pos++;
+            }
+        }
+        if (found) {
+            System.out.println("Found at position: " + pos);
+        } else {
+            System.out.println("Not found");
+        }
+        return pos;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Welcome to NUMERAL FINDER.com! Please enter a target value below: ");
+        int tgtVal = in.nextInt();
+
+        int[] array = new int[10000];
+
+        Arrays.sort(array);
+
+        Arrays.binarySearch(array, tgtVal);
+
+        for (int i = 1; i < array.length; i++) {
+
+            double r = (int) 10 * Math.random();
+
+            array[i] = (int) r;
+
+        }
+
+        System.out.println(Arrays.toString(array));
+
+        System.out.println(Arrays.binarySearch(array, tgtVal));
+
+        System.out.println(System.currentTimeMillis());
+
+
+        //When I had the array at 10 values, it takes 
+        //1703132383703 milliseconds. 
+
+        //When I had the array have 10,000 values, it takes
+        //1703132300436 milliseconds.
+
+        //When I increase the size of the array to 10,000,000 values, it takes
+        //1703132183108 milliseconds.
+
+
+    }
+}
---
+++ Unit06/HW15PartB/HW15PartB.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class HW15PartB {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        //Setting maximum arrays value equal to 20.
+        int[] numbers = new int[20];
+
+        int currentSize = 0;
+
+        while (true) {
+
+            //Asking user to input values.
+            System.out.print("Enter integer values or 'q' to quit below: ");
+
+            //if input has a "q" or a "Q", than the program quits and breaks. 
+            if (input.hasNext("q") || input.hasNext("Q")) {
+
+                input.next();
+
+                //Prints out string when user wants to quit. 
+                System.out.println("Done entering input.");
+
+                break;
+
+            } else {
+
+                //if current value is less than the length of the array, then keep on
+                //adding 1 to the currentSize. 
+                if (currentSize < numbers.length - 1) {
+                    numbers[currentSize] = input.nextInt();
+                    
+                    currentSize = currentSize + 1;
+                } else {
+
+                    numbers[currentSize] = input.nextInt();
+                    //If the amount of inputs given by the user reaches 20, 
+                    //print "Array is full!"
+                    System.out.println("Array is full!");
+
+                    //Then break.
+                    break;
+                }
+            }
+        }
+
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i <= currentSize; i++) {
+            if (i == currentSize) {
+                System.out.print(numbers[i]); 
+            } else {
+                System.out.print(numbers[i] + ", ");
+            }
+        }
+            
+        System.out.println();
+        System.out.println();
+
+        System.out.print("Enter a target value here: ");
+        int target_Val = input.nextInt();
+
+        int countVal = 0;
+
+        for (int i = 0; i <= currentSize; i++) {
+            if (numbers[i] == target_Val) {
+                countVal++;
+            }
+        }
+        System.out.println(countVal);
+
+        System.out.println();
+        System.out.println();
+
+        if (countVal > 0) {
+            System.out.println("The target value " + target_Val + " appears " + countVal + " time(s).");
+        } else {
+            System.out.println("The target value " + target_Val + " does not appear in the array.");
+        }
+    }
+}
+
+
